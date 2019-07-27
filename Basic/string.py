44eeef2de9f7cd9f254731ab207ecc50c7c4249f
blackboot03 <53190732+blackboot03@users.noreply.github.com>
2019-07-27 15:13:46 +0700

    list and string learning

---
+++ Basic/string.py
@@ -0,0 +1,16 @@
+text1 = """
+Guru    : "Kamu lagi belajar apa bloh??"
+Gembloh : " Lagi belajar python pak."
+"""
+
+print(text1)
+
+path = '/var/www/http'
+print(path)
+
+path2 = r'C:\belajar' # r = raw string
+print(path2)
+
+#print multiple
+print(10*"ha")
+
---
+++ Basic/string2.py
@@ -0,0 +1,8 @@
+text = "Pemrograman Python"
+
+a = text[0:11]
+b = text[-6:]
+
+print(a)
+print(b)
+print(a + " " + b)
