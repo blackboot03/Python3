583b70f1bc76d2cfc31a9396b33678b07f031853
blackboot03 <53190732+blackboot03@users.noreply.github.com>
2019-07-27 15:12:50 +0700

    learn python number

---
+++ Basic/number.py
@@ -0,0 +1,58 @@
+Data = [1,4,9,16,25,36,49]
+#
+# # menampilkan isi list
+#
+print(Data)
+#
+# # mengakses list
+#
+# SubData1 = Data[3]
+# SubData0 = Data[-3]
+# print(SubData0)
+# print(SubData1)
+#
+# # memotong list
+# SubData2 = Data[0:5]
+# SubData3 = Data[-5:]
+# SubData4 = Data[4:]
+# print(SubData2)
+# print(SubData3)
+# print(SubData4)
+
+# menambah list
+
+Data2 = [100,200,300,400,500,600,700]
+# Data3 = Data + Data2
+# print(Data3)
+
+# merubah konten list
+# Data[5] = 50
+# print(Data)
+
+# mengcopy list ke variable baru
+# a = Data[:]
+# a[5] = 100
+# print(a)
+
+# merubah konten dengan metode slicing
+# b = Data[:]
+# b[1:3] = [20,30]
+# print(b)
+
+# list dalam list
+# x = [Data,Data2]
+# print(x)
+
+#mengakses list multidimensional
+# y = x[1][1:4]
+# print(y)
+
+# methods list
+# Data.append(100)
+# print(Data)
+
+# list function
+
+panjang_list = len(Data)
+#print(Data2)
+print(panjang_list)
